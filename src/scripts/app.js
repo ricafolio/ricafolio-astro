66d16a3bae106a18193435d73f55ebcc6668c2e2
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-03-30 18:18:52 +0800

    fix: fix and load new script

---
+++ src/scripts/app.js
@@ -0,0 +1,30 @@
+import Swup from "swup"
+import SwupA11yPlugin from "@swup/a11y-plugin"
+import SwupHeadPlugin from "@swup/head-plugin"
+import SwupSlideTheme from "@swup/slide-theme"
+
+function initIndexEffect() {
+	if (window.location.pathname === '/') {
+		const rica = document.getElementById("rica")
+		const ph = document.getElementById("ph")
+		const emoji = document.getElementById("emoji")
+		const setEmoji = (emojicon) => {
+			emoji.innerHTML = emojicon
+		}
+		const resetEmoji = () => {
+			emoji.innerHTML = "👋"
+		}
+
+		rica.addEventListener("mouseenter", () => {setEmoji("🤠")}, false)
+		rica.addEventListener("mouseleave", resetEmoji, false)
+
+		ph.addEventListener("mouseenter", () => {setEmoji("🇵🇭")}, false)	
+		ph.addEventListener("mouseleave", resetEmoji, false)
+	}
+}
+
+const swup = new Swup({
+  plugins: [new SwupA11yPlugin(), new SwupHeadPlugin(), new SwupSlideTheme()]
+})
+
+swup.on('contentReplaced', initIndexEffect)
