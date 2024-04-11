22ffaa6005c7cb74680cf1556c457c20a82b64ff
ricafolio <29043464+ricafolio@users.noreply.github.com>
2024-04-11 15:52:01 +0800

    feat: remove swup in main script

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,38 +1,17 @@
-import Swup from "swup"
-import SwupA11yPlugin from "@swup/a11y-plugin"
-import SwupHeadPlugin from "@swup/head-plugin"
-import SwupSlideTheme from "@swup/slide-theme"
-
-function initIndexScript() {
-	if (window.location.pathname === '/') {
-		const rica = document.getElementById("rica")
-		const philippines = document.getElementById("ph")
-		const emoji = document.getElementById("emoji")
-		const setEmoji = (emojicon) => {
-			emoji.innerHTML = emojicon
-		}
-		const resetEmoji = () => {
-			emoji.innerHTML = "👋"
-		}
-
-		rica.addEventListener("mouseenter", () => {setEmoji("🤠")}, false)
-		rica.addEventListener("mouseleave", resetEmoji, false)
-
-		philippines.addEventListener("mouseenter", () => {setEmoji("🇵🇭")}, false)	
-		philippines.addEventListener("mouseleave", resetEmoji, false)
+if (window.location.pathname === '/') {
+	const rica = document.getElementById("rica")
+	const philippines = document.getElementById("ph")
+	const emoji = document.getElementById("emoji")
+	const setEmoji = (emojicon) => {
+		emoji.innerHTML = emojicon
 	}
+	const resetEmoji = () => {
+		emoji.innerHTML = "👋"
+	}
+
+	rica.addEventListener("mouseenter", () => { setEmoji("🤠") }, false)
+	rica.addEventListener("mouseleave", resetEmoji, false)
+
+	philippines.addEventListener("mouseenter", () => { setEmoji("🇵🇭") }, false)
+	philippines.addEventListener("mouseleave", resetEmoji, false)
 }
-
-const swup = new Swup({
-  plugins: [new SwupA11yPlugin(), new SwupHeadPlugin(), new SwupSlideTheme()]
-})
-
-// run once when page loads
-if (document.readyState === 'complete') {
-  initIndexScript();
-} else {
-  document.addEventListener('DOMContentLoaded', () => initIndexScript());
-}
-
-// run every page transition
-swup.on('contentReplaced', initIndexScript)
