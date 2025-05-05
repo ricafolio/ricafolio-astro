2149f468c032024bad09cfb5c60b16770f8916a9
ricafolio <29043464+ricafolio@users.noreply.github.com>
2025-05-05 09:26:14 +0800

    refactor: run prettier formatter

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Rica's Portfolio
+
 > 🌈 Hello friend, thanks for stopping by! <br/>
 > 🏠 <a href="https://ricafolio.dev/" target="_blank">https://ricafolio.dev</a>
 
 ### Built with
+
 - [Astro](https://docs.astro.build)
 - [Tailwind CSS](https://tailwindcss.com)
 
 ## Preview
+
 ![Preview screenshot](https://i.imgur.com/8RZGTJk.png)
 
 ## Old Version
+
 > Old version of my portfolio is also available here:
 > [old.ricafolio.dev](https://old.ricafolio.dev)
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,17 @@
 /** @type {import("tailwindcss").Config} */
 
 module.exports = {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			height: {
-				"80": "80vh"
-			},
-			fontFamily: {
-				"satoshi": ["Satoshi", "sans-serif"],
-				"clash": ["Clash Display", "sans-serif"],
-			}
-		}
-	},
-	plugins: []
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      height: {
+        80: "80vh",
+      },
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        clash: ["Clash Display", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
 }
