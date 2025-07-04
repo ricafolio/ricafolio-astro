57178b98c17b3d7db5a242f150e4b14316033d20
ricafolio <29043464+ricafolio@users.noreply.github.com>
2025-07-04 08:57:31 +0800

    fix: loading homepage script

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,27 +1,35 @@
-const rica = document.getElementById("rica")
-const philippines = document.getElementById("ph")
-const emoji = document.getElementById("emoji")
-const setEmoji = (emojicon) => {
-  emoji.innerHTML = emojicon
-}
-const resetEmoji = () => {
-  emoji.innerHTML = "👋"
+function loadIndexScript() {
+  const rica = document.getElementById("rica")
+  const philippines = document.getElementById("ph")
+  const emoji = document.getElementById("emoji")
+  const setEmoji = (emojicon) => {
+    emoji.innerHTML = emojicon
+  }
+  const resetEmoji = () => {
+    emoji.innerHTML = "👋"
+  }
+
+  rica.addEventListener(
+    "mouseenter",
+    () => {
+      setEmoji("🤠")
+    },
+    false,
+  )
+  rica.addEventListener("mouseleave", resetEmoji, false)
+
+  philippines.addEventListener(
+    "mouseenter",
+    () => {
+      setEmoji("🇵🇭")
+    },
+    false,
+  )
+  philippines.addEventListener("mouseleave", resetEmoji, false)
 }
 
-rica.addEventListener(
-  "mouseenter",
-  () => {
-    setEmoji("🤠")
-  },
-  false,
-)
-rica.addEventListener("mouseleave", resetEmoji, false)
-
-philippines.addEventListener(
-  "mouseenter",
-  () => {
-    setEmoji("🇵🇭")
-  },
-  false,
-)
-philippines.addEventListener("mouseleave", resetEmoji, false)
+document.addEventListener("astro:page-load", () => {
+  if (window.location.pathname === "/") {
+    loadIndexScript()
+  }
+})
